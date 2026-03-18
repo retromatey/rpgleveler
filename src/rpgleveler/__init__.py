5f933ccdf4bbce3d0840a1b8ed3ac251ff3bbc6b
Jason Tennant <jason.tennant@outlook.com>
2026-03-18 06:35:22 -0700

    cleaning out old text related to the rpgcharacter project

--- src/rpgleveler/__init__.py
+++ src/rpgleveler/__init__.py
@@ -1,25 +1,11 @@
 """
-Basic Fantasy RPG character generation package.
-
-This package provides tools for generating level-1 characters using Basic
-Fantasy rules, including ability rolling, race and class validation, and derived
-combat statistics.
+TODO: add module comments
 """
 
-from .character_generator import (
-    AbilityScores,
-    Character,
-    generate_character,
-    roll_abilities,
+from .character_leveler import (
 )
 from .classes import ClassName
-from .races import RaceName
 
 __all__ = [
-    "AbilityScores",
-    "Character",
     "ClassName",
-    "RaceName",
-    "generate_character",
-    "roll_abilities",
 ]
